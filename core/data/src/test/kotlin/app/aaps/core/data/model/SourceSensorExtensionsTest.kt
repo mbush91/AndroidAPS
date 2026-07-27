b9c3e5055f1aae23e2b55aaba05221fd2e24f5ce
mbush91 <mbush1@ltu.edu>
2026-07-27 15:11:12 -0400

    Fix BYOESA SMB trust persistence compatibility

--- core/data/src/test/kotlin/app/aaps/core/data/model/SourceSensorExtensionsTest.kt
+++ core/data/src/test/kotlin/app/aaps/core/data/model/SourceSensorExtensionsTest.kt
@@ -35,13 +35,8 @@ class SourceSensorExtensionsTest {
     }
 
     @Test
-    fun `eversense does not support advanced filtering`() {
-        assertThat(SourceSensor.EVERSENSE.advancedFilteringSupported()).isFalse()
-    }
-
-    @Test
-    fun `BYOESA supports advanced filtering`() {
-        assertThat(SourceSensor.BYOESA.advancedFilteringSupported()).isTrue()
+    fun `BYOESA eversense source supports advanced filtering`() {
+        assertThat(SourceSensor.EVERSENSE.advancedFilteringSupported()).isTrue()
     }
 
     @Test
